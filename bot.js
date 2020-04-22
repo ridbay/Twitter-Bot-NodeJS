a850347afeceee34c99a59c3b762b37c2eb16763
Ridwan Balogun <balogunridwan@gmail.com>
2020-04-22 19:53:10 +0100

    Twitter Bot

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 // Dependencies =========================
 let twit = require('twit'),
-  config = require('./config');
+    config = require('./config');
 
 let Twitter = new twit(config);
 
@@ -8,40 +8,40 @@ let Twitter = new twit(config);
 
 // find latest tweet according the query 'q' in params
 let retweet = function () {
-  let params = {
-    q: '#nodejs, #Nodejs', // REQUIRED
-    result_type: 'recent',
-    lang: 'en'
-  };
-  Twitter.get('search/tweets', params, function (err, data) {
-    // if there no errors
-    if (!err) {
-      // grab ID of tweet to retweet
-      let retweetId = data.statuses[0].id_str;
-      // Tell TWITTER to retweet
-      Twitter.post(
-        'statuses/retweet/:id',
-        {
-          id: retweetId
-        },
-        function (err, response) {
-          if (response) {
-            console.log('Retweeted!!!');
-          }
-          // if there was an error while tweeting
-          if (err) {
-            console.log(
-              'Something went wrong while RETWEETING... Duplication maybe...'
+    let params = {
+        q: '#nodejs, #Nodejs', // REQUIRED
+        result_type: 'recent',
+        lang: 'en'
+    };
+    Twitter.get('search/tweets', params, function (err, data) {
+        // if there no errors
+        if (!err) {
+            // grab ID of tweet to retweet
+            let retweetId = data.statuses[0].id_str;
+            // Tell TWITTER to retweet
+            Twitter.post(
+                'statuses/retweet/:id',
+                {
+                    id: retweetId
+                },
+                function (err, response) {
+                    if (response) {
+                        console.log('Retweeted!!!');
+                    }
+                    // if there was an error while tweeting
+                    if (err) {
+                        console.log(
+                            'Something went wrong while RETWEETING... Duplication maybe...'
+                        );
+                    }
+                }
             );
-          }
         }
-      );
-    }
-    // if unable to Search a tweet
-    else {
-      console.log('Something went wrong while SEARCHING...');
-    }
-  });
+        // if unable to Search a tweet
+        else {
+            console.log('Something went wrong while SEARCHING...');
+        }
+    });
 };
 
 // grab & retweet as soon as program is running...
@@ -53,33 +53,33 @@ setInterval(retweet, 3000000);
 
 // find a random tweet and 'favorite' it
 let favoriteTweet = function () {
-  let params = {
-    q: '#nodejs, #Nodejs', // REQUIRED
-    result_type: 'recent',
-    lang: 'en'
-  };
-  // find the tweet
-  Twitter.get('search/tweets', params, function (err, data) {
-    // find tweets
-    let tweet = data.statuses;
-    let randomTweet = ranDom(tweet); // pick a random tweet
+    let params = {
+        q: '#nodejs, #Nodejs', // REQUIRED
+        result_type: 'recent',
+        lang: 'en'
+    };
+    // find the tweet
+    Twitter.get('search/tweets', params, function (err, data) {
+        // find tweets
+        let tweet = data.statuses;
+        let randomTweet = ranDom(tweet); // pick a random tweet
 
-    // if random tweet exists
-    if (typeof randomTweet != 'undefined') {
-      // Tell TWITTER to 'favorite'
-      Twitter.post('favorites/create', { id: randomTweet.id_str }, function (
-        err,
-        response
-      ) {
-        // if there was an error while 'favorite'
-        if (err) {
-          console.log('CANNOT BE FAVORITE... Error');
-        } else {
-          console.log('FAVORITED... Success!!!');
+        // if random tweet exists
+        if (typeof randomTweet != 'undefined') {
+            // Tell TWITTER to 'favorite'
+            Twitter.post('favorites/create', { id: randomTweet.id_str }, function (
+                err,
+                response
+            ) {
+                // if there was an error while 'favorite'
+                if (err) {
+                    console.log('CANNOT BE FAVORITE... Error');
+                } else {
+                    console.log('FAVORITED... Success!!!');
+                }
+            });
         }
-      });
-    }
-  });
+    });
 };
 // grab & 'favorite' as soon as program is running...
 favoriteTweet();
@@ -88,6 +88,6 @@ setInterval(favoriteTweet, 3600000);
 
 // function to generate a random tweet tweet
 function ranDom(arr) {
-  let index = Math.floor(Math.random() * arr.length);
-  return arr[index];
+    let index = Math.floor(Math.random() * arr.length);
+    return arr[index];
 }
--- config.js
+++ config.js
@@ -2,5 +2,5 @@ module.exports = {
     consumerconsumer_key: 'CT7iPYl5pmzcrw13VxILApmYa',
     consumerconsumer_secret: '0zPsg0phS9IaruwTuVN7yjYEd8yp0Jlx60Il3EmQaj8jKBbTfT',
     accessaccess_token: '83565055-xWQARickBBROz44IQMrPRhsXdWMqKROiyRxQc6jah',
-    accessaccess_tokenaccess_token_secret: 'zx9Fa2wnlbgIr3dc1U3JQeu6z5YDI4gkYOCvpB99V3cEx' 
+    accessaccess_tokenaccess_token_secret: 'zx9Fa2wnlbgIr3dc1U3JQeu6z5YDI4gkYOCvpB99V3cEx'
 }
